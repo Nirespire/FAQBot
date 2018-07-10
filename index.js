c40f780d4e5b24d6232940fdfbb7d838bf849897
Sanjay Nair <sanjayhnair@gmail.com>
2018-07-09 21:55:31 -0400

    Added function comments and condensed some code

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Library to provide natual language processing and
+// basic machine learning cabailities
 const NLP = require('natural');
+
+// Provides a wrapper around the Slack API to easily
+// interact with the chat service
 const BotKit = require('botkit');
+
+// Built-in Node library for loading files operations
 const fs = require('fs');
 
 // Load our environment variables from the .env file
@@ -25,12 +32,25 @@ const Bot = BotKit.slackbot({
     storage: undefined
 });
 
+/**
+ * Function to easily parse a given json file to a JavaScript Object
+ * 
+ * @param {String} filePath 
+ * @returns {Object} Object parsed from json file provided
+ */
 function parseTrainingData(filePath) {
     const trainingFile = fs.readFileSync('./trainingData.json');
     return JSON.parse(trainingFile);
 }
 
-function train(classifier, label, phrases) {
+/**
+ * Will add the phrases to the provided classifier under the given label.
+ * 
+ * @param {Classifier} classifier
+ * @param {String} label
+ * @param {Array.String} phrases
+ */
+function trainClassifier(classifier, label, phrases) {
     console.log('Teaching set', label, phrases);
     phrases.forEach((phrase) => {
         console.log(`Teaching single ${label}: ${phrase}`);
@@ -38,19 +58,16 @@ function train(classifier, label, phrases) {
     });
 }
 
-function createClassifier() {
-    classifier.train();
-
-    const filePath = './classifier.json';
-
-    classifier.save(filePath, (err, classifier) => {
-        if (err) {
-            console.error(err);
-        }
-        console.log('Created a Classifier file in ', filePath);
-    });
-}
-
+/**
+ * Uses the trained classifier to give a prediction of what
+ * labels the provided pharse belongs to with a confidence
+ * value associated with each and a a guess of what the actual
+ * label should be based on the minConfidence threshold.
+ * 
+ * @param {String} phrase 
+ * 
+ * @returns {Object}
+ */
 function interpret(phrase) {
     console.log('interpret', phrase);
     const guesses = classifier.getClassifications(phrase.toLowerCase());
@@ -64,7 +81,18 @@ function interpret(phrase) {
 
 
 
-// Function to handle an incoming message
+/**
+ * Callback function for BotKit to call. Provided are the speech
+ * object to reply and the message that was provided as input.
+ * Function will take the input message, attempt to label it 
+ * using the trained classifier, and return the corresponding
+ * answer from the training data set. If no label can be matched
+ * with the set confidence interval, it will respond back saying
+ * the message was not able to be understood.
+ * 
+ * @param {Object} speech 
+ * @param {Object} message 
+ */
 function handleMessage(speech, message) {
     const interpretation = interpret(message.text);
     console.log('InternChatBot heard: ', message.text);
@@ -82,12 +110,21 @@ function handleMessage(speech, message) {
 // Load our training data
 const trainingData = parseTrainingData("./trainingData.json");
 
+// For each of the labels in our training data,
+// train and generate the classifier
 var i = 0;
 Object.keys(trainingData).forEach((element, key) => {
-    train(classifier, element, trainingData[element].questions);
+    trainClassifier(classifier, element, trainingData[element].questions);
     i++;
     if (i === Object.keys(trainingData).length) {
-        createClassifier(classifier);
+        classifier.train();
+        const filePath = './classifier.json';
+        classifier.save(filePath, (err, classifier) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log('Created a Classifier file in ', filePath);
+        });
     }
 });
 
